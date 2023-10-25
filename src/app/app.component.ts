7050657bf538a08cf141a0ae8b904d8c6a6a4db5
egemen-c <egemen.celik@pia-team.com>
2023-10-25 21:34:08 +0300

    feat: added categories component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component} from '@angular/core';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { ButtonComponent } from './components/button/button.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [NavigationComponent, ButtonComponent],
+    imports: [NavigationComponent, ButtonComponent, CategoriesComponent],
 })
 export class AppComponent {
   buttonStyle = 'width:220px; height:75px; color:white; padding: 25px 50px; background-color: var(--primary);'
---
+++ src/app/components/categories/categories.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriesService } from 'src/app/services/categories.service';
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.scss']
+})
+
+export class CategoriesComponent implements OnInit {
+  categories: String[] = [];
+  
+  constructor(private categoriesService: CategoriesService) {}
+  ngOnInit() {
+    this.categoriesService.getAllCategories().subscribe( response => {
+      this.categories = response.slice(1, 4)
+    })
+  }
+}
