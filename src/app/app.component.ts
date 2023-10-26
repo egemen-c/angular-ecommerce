0094992a9633f65baf634b16aed42011a08d9283
egemen-c <egemen.celik@pia-team.com>
2023-10-26 15:09:13 +0300

    fix: fixed ngSrc attiribute error.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component} from '@angular/core';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { ButtonComponent } from './components/button/button.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { NgOptimizedImage } from '@angular/common';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [NavigationComponent, ButtonComponent, CategoriesComponent],
+    imports: [NavigationComponent, ButtonComponent, CategoriesComponent, NgOptimizedImage],
 })
 export class AppComponent {
   buttonStyle = 'width:220px; height:75px; color:white; padding: 25px 50px; background-color: var(--primary);'
